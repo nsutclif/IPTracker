ec3b6b53eadf38b309fa173a86f093571267f7b4
nsutclif <nscodio@nathansutcliffe.net>
2014-06-10 03:40:47 +0000

    merging into one file

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 var http = require("http"),
     mongojs = require("mongojs");
+    url = require("url");
 
 var uri = process.env.MONGO_URI || "mongodb://devtest:trackerdev@ds031988.mongolab.com:31988/nsiptracker_dev",
     db = mongojs.connect(uri);
-
-var server = http.createServer(requestHandler);
 
 function requestHandler(request, response) {
     response.writeHead(200, {"Content-Type": "text/html"});
     
     logged_ipsCollection = db.collection('logged_ips');
     
-    logged_ipsCollection.find({}, function(err, records) {
+    var url_parts = url.parse(request.url, true);
+    var query = url_parts.query;
+    console.log(query);
+    
+    var computername = query.computername;
+    
+    if(computername){    
+        var document = {
+            computername:computername,
+            ip:request.connection.remoteAddress,
+            servertime:new Date()
+        };
+    
+        console.log(document);
+        logged_ipsCollection.insert(document);
+    }
+
+    logged_ipsCollection.find({}, {limit:5,sort:[['servertime',-1]]}, function(err, records) {
         if(err) {
             console.log("There was an error executing the database query.");
             response.end();
@@ -21,15 +37,25 @@ function requestHandler(request, response) {
         i = records.length;
 
         while(i--) {
-            html += '<p><b>IP:</b> ' 
-                 + records[i].ip 
+            html += '<p><b>Computer Name:</b> ' 
+                 + records[i].computername 
+                 + ' <br /><b>IP:</b> ' 
+                 + records[i].ip
                  + ' <br /><b>Server Time:</b> ' 
                  + records[i].servertime
         }
+        
+        html += '<form>';
+        html += 'Computer Name: <input type="text" name="computername">';
+        html += '<input type="submit" value="Log IP">';
+        html += '</form>'
+        
         response.write(html);
         response.end();                            
     });
 }
+
+var server = http.createServer(requestHandler);
 
 var port = Number(process.env.PORT || 3000);
 
